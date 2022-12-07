1495394e7cb774b4e55ec0c52338e9d05b8e696b
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-12-07 20:29:47 -0300

    Create bossPirata.java

---
+++ RpgBeta0_0.1/src/Monstros/bossPirata.java
@@ -0,0 +1,10 @@
+package Monstros;
+
+public class bossPirata extends SuperMonstros{
+	public goblin() {
+		nomeMonstro = "Pirata Rei";
+		vidaMonstro = 10;
+		danoMonstro = 5;
+		monstroAtaqueMensagem = "O Pirata rei bateu em você com uma espada";
+	}
+}
