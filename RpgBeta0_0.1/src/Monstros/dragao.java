5b3bd69217c1e6a317bf79a934a8c3eb0838a8d3
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-12-10 15:57:21 -0300

    Create dragao.java

---
+++ RpgBeta0_0.1/src/Monstros/dragao.java
@@ -0,0 +1,10 @@
+package Monstros;
+
+public class dragao extends SuperMonstros{
+	public goblin() {
+		nomeMonstro = "Dragão";
+		vidaMonstro = 20;
+		danoMonstro = 50;
+		monstroAtaqueMensagem = "O Dragão jogou suas chamas em você";
+	}
+}
